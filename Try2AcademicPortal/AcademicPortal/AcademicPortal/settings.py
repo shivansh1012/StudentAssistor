2e5bc0d88fbd9263f9d2be3fef9ca9dd2b5cd8ff
sakar900 <65478431+sakar900@users.noreply.github.com>
2020-11-05 01:09:51 +0530

    Added Fileupload app

--- Try2AcademicPortal/AcademicPortal/AcademicPortal/settings.py
+++ Try2AcademicPortal/AcademicPortal/AcademicPortal/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'fileuploadapp.apps.FileuploadappConfig',
 
     'calendarapp',
     'dashboard',
@@ -125,3 +126,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS= [ BASE_DIR / 'static']
+
+MEDIA_URL='/media/'
+
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
