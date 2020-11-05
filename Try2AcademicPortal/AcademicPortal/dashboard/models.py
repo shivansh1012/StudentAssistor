34bd2ac01bff38d2aef43f9f1f6db8d9708601f4
piyush-0120 <piyushsharma1420@gmail.com>
2020-11-05 22:41:34 +0530

    Added user settings and profile page

--- Try2AcademicPortal/AcademicPortal/dashboard/models.py
+++ Try2AcademicPortal/AcademicPortal/dashboard/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+class AutoDateTimeField(models.DateTimeField):
+    def pre_save(self, model_instance, add):
+        return timezone.now()
+
+class Student(models.Model):
+    name = models.CharField(max_length=256,null=True)
+    email = models.EmailField(max_length=256,null=True)
+    date_created = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
+    
+class 
